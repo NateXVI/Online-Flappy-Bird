3a132db6ffcdb74f7505142ed09586ee4960c802
Nate Diven <natediven1@gmail.com>
2020-10-20 18:01:02 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const { Client } = require('pg');
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+
+client.connect()
+.then(() => console.log('Connected to database'))
+
 app.get('/', function (req, res) {
     res.send("<h1>It's working.</h1>");
 })
+
+app.get('/api/get/leaderboard', function (req, res) {
+    client.query('SELECT * FROM scores')
+    .then(results => res.send(results));
+});
 
 app.listen(process.env.PORT || 3000, () => console.log('Server is running'));
